e78712b4422838ea6eac6b078a5b7fe7c3e99719
Ria Sommerfeld <sommerfeldria15@gmail.com>
2024-02-04 19:23:55 +0100

    data construction started

---
+++ app/src/main/java/com/natsu/nestsync/Collection.java
@@ -0,0 +1,10 @@
+package com.natsu.nestsync;
+
+import java.util.UUID;
+
+public class Collection {
+    public Collection (String name){
+        uID = UUID.randomUUID().toString();
+        collectionName = name;
+    }
+}
---
+++ app/src/main/java/com/natsu/nestsync/Globals.java
@@ -0,0 +1,14 @@
+package com.natsu.nestsync;
+
+public class Globals {
+    // nicht instanziierbarer Singelton, um Daten zu speichern, die über die ganze Klasse zugreifbar sein sollen
+    private static final Globals instance = new Globals();
+
+    private Globals() {
+        //hier Daten aus de Datenbank einlesen bzw Testdaten hart einkodieren
+    }
+
+    public static Globals getInstance() {
+        return instance;
+    }
+}
