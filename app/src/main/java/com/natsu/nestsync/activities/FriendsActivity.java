0913e53f0ab15eb33b848532e38ead67a7ce098d
Ria Sommerfeld <sommerfeldria15@gmail.com>
2024-03-09 23:44:21 +0100

    friends are displayed

--- app/src/main/java/com/natsu/nestsync/activities/FriendsActivity.java
+++ app/src/main/java/com/natsu/nestsync/activities/FriendsActivity.java
@@ -1,13 +1,17 @@
 package com.natsu.nestsync.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,15 +21,21 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.natsu.nestsync.FriendsAdapter;
+import com.natsu.nestsync.HomeAdapter;
+import com.natsu.nestsync.OnRecyclerItemClickListener;
 import com.natsu.nestsync.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class FriendsActivity extends AppCompatActivity {
+public class FriendsActivity extends AppCompatActivity implements OnRecyclerItemClickListener {
+    ArrayList<String> friendNames, friendIDs;
     Button backToMenuBtn;
     EditText addFriendEdit;
     FloatingActionButton addFriendBtn;
+    FriendsAdapter friendAdapt;
+    RecyclerView recView;
 
     DatabaseReference dataRef;
     FirebaseUser fUser;
@@ -36,14 +46,55 @@ public class FriendsActivity extends AppCompatActivity {
         backToMenuBtn = findViewById(R.id.toMenuButton);
         addFriendEdit = findViewById(R.id.addUserEditText);
         addFriendBtn = findViewById(R.id.addUserButton);
+        recView = findViewById(R.id.friendsList);
 
         fUser = FirebaseAuth.getInstance().getCurrentUser();
         dataRef = FirebaseDatabase.getInstance().getReference().child("users");
+
+        recView.hasFixedSize();
+        recView.setLayoutManager(new LinearLayoutManager(this));
+        friendNames = new ArrayList<>();
+        friendIDs = new ArrayList<>();
+        friendAdapt = new FriendsAdapter((Context) this, friendNames, friendIDs, (OnRecyclerItemClickListener) this);
+        recView.setAdapter(friendAdapt);
 
         backToMenuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),MenuActivity.class));
+            }
+        });
+
+        //query to display users friends
+        dataRef.child(fUser.getUid()).child("friends").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                friendNames.clear();
+                friendIDs.clear();
+                for (DataSnapshot snapshot1: snapshot.getChildren()){
+                    String friendID = snapshot1.getKey();
+                    friendIDs.add(friendID);
+
+                    dataRef.child(fUser.getUid()).child("friends").child(friendID).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            String friendName = dataSnapshot.getValue(String.class);
+                            friendNames.add(friendName);
+                            friendAdapt.notifyDataSetChanged();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Toast.makeText(FriendsActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                friendAdapt.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
@@ -80,5 +131,20 @@ public class FriendsActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    @Override
+    public void onRecItemClick(int pos, String id) {
+
+    }
+
+    @Override
+    public void onRecItemBtnClick(int pos, String id) {
+
+    }
+
+    @Override
+    public void onRecItemTextClick(String id, String text) {
+
     }
 }
